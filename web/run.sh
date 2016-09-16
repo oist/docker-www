2cf9930aea5ff43cc2f08c968d4fa56011f0c3f5
Pedro Cambra <pedro.cambra@gmail.com>
2016-09-16 16:43:54 +0900

    Added run.sh script for web container

---
+++ web/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Sort of semaphore process to ensure that the db is there when we run the drush
+# commands.
+BOOTSTRAP=`drush status | grep "Drupal bootstrap                :  Successful" | wc -l`
+until [ "$BOOTSTRAP" -ge "1" ]; do
+  >&2 echo "Waiting for the database to be ready"
+  BOOTSTRAP=`drush status | grep "Drupal bootstrap                :  Successful" | wc -l`
+  sleep 20
+done
+>&2 echo "Running drush commands"
+
+# Dev environment operations.
+drush -y en stage_file_proxy devel
+drush vset stage_file_proxy_origin 'https://www.oist.jp'
+drush vset stage_file_proxy_use_imagecache_root 0
+# Replace by a generic one
+drush sqlq "UPDATE users SET mail = 'pedro.cambra@oist.jp' WHERE uid = 1"
+echo "--------------------------------------------------------------------------"
+echo "****************************** Here's a uli ******************************"
+drush uli
+echo "--------------------------------------------------------------------------"
+
+# Run httpd process
+/usr/sbin/apachectl -D FOREGROUND
